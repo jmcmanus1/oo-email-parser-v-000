2854732f40927b1a595c662aaf64b5cc07ea1fd3
Julie  McManus <julieannesays@gmail.com>
2020-02-04 21:14:38 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -3,8 +3,8 @@ require 'pry'
 class EmailParser
   attr_accessor :email_addresses
 
-  def parse
-    @email_addresses
+  def parse(email_addresses)
+    @email_addresses = email_addresses
   end
 
 end
